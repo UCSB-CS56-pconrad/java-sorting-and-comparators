ea226d56853de88d1c7cc1e9e9c79241ae19c003
Phill Conrad <pconrad@cs.ucsb.edu>
2018-10-25 16:17:17 -0700

    rename to better names

---
+++ CanSortTheseDogs/SortDogs.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class SortDogs {
+    
+    public static void main(String [] args) {
+	ArrayList<Dog> kennel = new ArrayList<Dog>();
+	
+	kennel.add(new Dog("Fido",15));
+	kennel.add(new Dog("Spot",20));
+	kennel.add(new Dog("Puddles",8));
+	kennel.add(new Dog("Doge",45));
+	kennel.add(new Dog("Catepillar",90));
+	
+	System.out.println("Not sorted: " + kennel);
+	java.util.Collections.sort(kennel);
+	System.out.println("Sorted by name " + kennel);	
+    }   
+}
